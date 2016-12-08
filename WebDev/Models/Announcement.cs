958afb737f6a90a68d8f37a9f6a54447acf4805c
carlo <carlo@DESKTOP-3I05RQ0>
2016-12-08 13:17:07 +0000

    Add project files.

---
+++ WebDev/Models/Announcement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebDev.Models
+{
+    public class Announcement
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public DateTime DandT { get; set; }
+        public String Author { get; set; }
+        public string Content { get; set; }
+        //link announcement to user
+        public virtual ApplicationUser User { get; set; }
+    }
+}
