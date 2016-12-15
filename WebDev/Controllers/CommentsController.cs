c7009f427964dcb119730d960be643a23b4fbaa0
carlo <carloernestosarli@gmail.com>
2016-12-15 10:36:12 +0000

    added comments trying to link them

--- WebDev/Controllers/CommentsController.cs
+++ WebDev/Controllers/CommentsController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNet.Identity;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
@@ -11,120 +10,107 @@ using WebDev.Models;
 
 namespace WebDev.Controllers
 {
-    public class AnnouncementsController : Controller
+    public class CommentsController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: Announcements
+        // GET: Comments
         public ActionResult Index()
         {
-            return View(db.Announcements.ToList());
+            return View(db.Comments.ToList());
         }
 
-
-        public ActionResult BuildAnnouncementTable()
-        {
-            return PartialView("_AnnouncementsTable", db.Announcements.ToList());
-        }
-        // GET: Announcements/Details/5
+        // GET: Comments/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Announcement announcement = db.Announcements.Find(id);
-            if (announcement == null)
+            Comment comment = db.Comments.Find(id);
+            if (comment == null)
             {
                 return HttpNotFound();
             }
-
-            AnnouncementView AV = populateAnnouncementViewModel(id);
-            return View(AV);
+            return View(comment);
         }
 
-        // GET: Announcements/Create
+        // GET: Comments/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Announcements/Create
+        // POST: Comments/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Title,DandT,Author,Content")] Announcement announcement)
+        public ActionResult Create([Bind(Include = "CommentId,DandT,Author,Content,AnnouncementId")] Comment comment)
         {
             if (ModelState.IsValid)
             {
-                //get user identity 
-                string currentUserId = User.Identity.GetUserId();
-                ApplicationUser currentUser = db.Users.FirstOrDefault(
-                    x => x.Id == currentUserId);
-                announcement.User = currentUser;
-
-                db.Announcements.Add(announcement);
+                db.Comments.Add(comment);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(announcement);
+            return View(comment);
         }
 
-        // GET: Announcements/Edit/5
+        // GET: Comments/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Announcement announcement = db.Announcements.Find(id);
-            if (announcement == null)
+            Comment comment = db.Comments.Find(id);
+            if (comment == null)
             {
                 return HttpNotFound();
             }
-            return View(announcement);
+            return View(comment);
         }
 
-        // POST: Announcements/Edit/5
+        // POST: Comments/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Title,DandT,Author,Content")] Announcement announcement)
+        public ActionResult Edit([Bind(Include = "CommentId,DandT,Author,Content,AnnouncementId")] Comment comment)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(announcement).State = EntityState.Modified;
+                db.Entry(comment).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(announcement);
+            return View(comment);
         }
 
-        // GET: Announcements/Delete/5
+        // GET: Comments/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Announcement announcement = db.Announcements.Find(id);
-            if (announcement == null)
+            Comment comment = db.Comments.Find(id);
+            if (comment == null)
             {
                 return HttpNotFound();
             }
-            return View(announcement);
+            return View(comment);
         }
 
-        // POST: Announcements/Delete/5
+        // POST: Comments/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Announcement announcement = db.Announcements.Find(id);
-            db.Announcements.Remove(announcement);
+            Comment comment = db.Comments.Find(id);
+            db.Comments.Remove(comment);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
@@ -137,12 +123,5 @@ namespace WebDev.Controllers
             }
             base.Dispose(disposing);
         }
-
-        //populate announcements with comments
-        public void populateAnnouncementViewModel(int id)
-        {
-
-        }
-
     }
 }
---
+++ WebDev/Models/AnnouncementView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebDev.Models
+{
+    public class AnnouncementView
+    {
+        public Announcement Announcement;
+        public List<Comment> Comments;
+    }
+}
